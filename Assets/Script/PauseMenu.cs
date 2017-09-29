c26b90bb4e2733704efad30a7ee9a3c01eb8f31c
Louis <louis.christophe.poulin@gmail.com>
2017-09-28 23:20:13 -0400

    Ajout du menu inventaire

--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -4,36 +4,80 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour {
 
-    public bool isPaused;
+    public enum MenuStates { None, Main, Inventory}
+    MenuStates states;
 
     public GameObject pauseMenu;
+    public GameObject inventoryMenu;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(isPaused)
+    
+
+    // Use this for initialization
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        switch(states)
         {
-            pauseMenu.SetActive(true);
-            Time.timeScale = 0f;
+            case MenuStates.None:
+                pauseMenu.SetActive(false);
+                inventoryMenu.SetActive(false);
+                break;
+
+            case MenuStates.Main:
+                pauseMenu.SetActive(true);
+                inventoryMenu.SetActive(false);
+                break;
+
+            case MenuStates.Inventory:
+                pauseMenu.SetActive(false);
+                inventoryMenu.SetActive(true);
+                break;
         }
-        else
+        //if (isPaused)
+        //{
+        //    pauseMenu.SetActive(true);
+        //    Time.timeScale = 0f;
+        //}
+        //else
+        //{
+        //    pauseMenu.SetActive(false);
+        //    Time.timeScale = 1f;
+        //}
+
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            pauseMenu.SetActive(false);
-            Time.timeScale = 1f;
+            if (states == MenuStates.Main)
+                states = MenuStates.None;
+
+            if (states == MenuStates.None)
+                states = MenuStates.Main;
         }
 
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            isPaused = !isPaused;
-        }
-	}
+        //if(isInventaire)
+        //{
+        //    //pauseMenu.SetActive(false);
+        //    inventaire.isOpen = true;
+        //    isPaused = false;
+        //    //inventaireMenu.SetActive(true);
+
+        //}
+    }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void Open_Inventory()
+    {
+        states = MenuStates.Inventory;
+    }
+
+    public void Retour_Menu_Inventory()
+    {
+        states = MenuStates.Main;
     }
 }
