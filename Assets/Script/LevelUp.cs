46a48115711c0f10e92327621fa10d15fbd6ada7
Unknown <etiennesimoneau@hotmail.fr>
2017-10-10 13:49:29 -0400

    commit4Etien

--- Assets/Script/LevelUp.cs
+++ Assets/Script/LevelUp.cs
@@ -1,117 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Mono.Data.Sqlite;
 using UnityEngine;
 using UnityEngine.UI;
-
-
-
-public class LevelUp : MonoBehaviour {
-
-    public GameObject Menu;
-
-    public Text textRemainingSouls;
-
-    public int souls;
-    public Text soulsRemaining;
-
-    public Button buttonForce;
-    public Button buttonDef;
-    public Button buttonSpeed;
-    public Button exitButton;
-
-    private int forceValue;
-    private int DefenseValue;
-    private int SpeedValue;
-    private int soulsNumber;
-
-    // Use this for initialization
-    void Start () {
-
-        AccesBD bd = new AccesBD();
-        IDataReader reader;
-        reader = bd.select("select * from Stats where Stats.idStats = 1;");
-
-        while (reader.Read())
-        {
-            forceValue = reader.GetInt32(2);
-            DefenseValue = reader.GetInt32(3);
-            SpeedValue = reader.GetInt32(4);
-            soulsNumber = reader.GetInt32(5);
-        }
-
-        // Get de la bd
-        //forceValue = 2;
-        //DefenseValue = 3;
-        //SpeedValue = 1;
-        //soulsNumber = 2;
-        UpdateUI();
-
-        buttonForce.onClick.AddListener(onForceClick);
-        buttonDef.onClick.AddListener(onDefenseClick);
-        buttonSpeed.onClick.AddListener(onSpeedClick);
-        exitButton.onClick.AddListener(exitLevelMenu);
-    }
-	
-	// Update is called once per frame
-	void Update () {
-    }
-
-    public void UpdateUI()
-    {
-        textRemainingSouls.text = soulsNumber.ToString();
-
-        sliderForce.value = forceValue;
-        sliderDef.value = DefenseValue;
-        sliderSpeed.value = SpeedValue;
-    }
-
-    public void onForceClick()
-    {
-        if (forceValue < 600 && soulsNumber > 0)
-        {
-            forceValue += 100;
-            soulsNumber--;
-            UpdateUI();
-        }
-    }
-
-    private void onDefenseClick()
-    {
-        if (DefenseValue < 600 && soulsNumber > 0)
-        {
-            DefenseValue += 100;
-            soulsNumber--;
-            UpdateUI();
-        }
-    }
-
-    private void onSpeedClick()
-    {
-        if (SpeedValue < 600 && soulsNumber > 0)
-        {
-            SpeedValue += 100;
-            soulsNumber--;
-            UpdateUI();
-        }
-    }
-
-    public void OpenLevelMenu()
-    {
-        Menu.SetActive(true);
-    }
-
-    private void exitLevelMenu()
-    {
-        Menu.SetActive(false);
-        // Mettre à jour la bd...
-        // Fermer la bd.
-    }
-
-    public void addSouls(int theSouls)
-    {
-        soulsNumber += theSouls;
-    }
-}
+using System.Data;
+using Assets.Script;
 
 
 
@@ -120,6 +13,11 @@ public class LevelUp : MonoBehaviour
     public GameObject Menu;
 
     public Text textRemainingSouls;
+
+    public Slider sliderForce;
+    public Slider sliderDef;
+    public Slider sliderSpeed;
+
     public Button buttonForce;
     public Button buttonDef;
     public Button buttonSpeed;
@@ -138,7 +36,7 @@ public class LevelUp : MonoBehaviour
         AccesBD bd = new AccesBD();
 
         try
-        {  
+        {
             IDataReader reader;
             reader = bd.select("select * from Stats where Stats.idStats = 2"); // *voir quelle personnage sera le main*
 
@@ -168,6 +66,8 @@ public class LevelUp : MonoBehaviour
             bd.Close();
             Debug.Log("bd fermer");
         }
+    }
+
     public void UpdateUI()
     {
         textRemainingSouls.text = soulsNumber.ToString();
@@ -178,6 +78,7 @@ public class LevelUp : MonoBehaviour
     }
 
     public void onForceClick()
+    {
         if (forceValue < 600 && soulsNumber > 0)
         {
             bdModifier = true;
@@ -185,6 +86,7 @@ public class LevelUp : MonoBehaviour
             soulsNumber--;
             UpdateUI();
         }
+    }
 
     private void onDefenseClick()
     {
@@ -239,3 +141,4 @@ public class LevelUp : MonoBehaviour
     {
         soulsNumber += theSouls;
     }
+}
