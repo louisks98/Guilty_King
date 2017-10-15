6de19cb8ba3db479aa6a14d3a999ad3f58c6b71c
Unknown <louis.christophe.poulin@gmail.com>
2017-10-15 14:41:33 -0400

    update de affichage stats

--- Assets/Script/MenuStatTeam.cs
+++ Assets/Script/MenuStatTeam.cs
@@ -9,6 +9,10 @@ public class MenuStatTeam : MonoBehaviour {
 
     private List<Personnage> listPers = new List<Personnage>();
     public List<GameObject> characterPanels = new List<GameObject>();
+    public Sprite sp_hero;
+    public Sprite sp_Fire;
+    public Sprite sp_Forest;
+    public Sprite sp_Ice;
 
     // Use this for initialization
     void Start() {
@@ -30,17 +34,24 @@ public class MenuStatTeam : MonoBehaviour {
     {
         AccesBD bd = new AccesBD();
         SqliteDataReader reader;
-        reader = bd.select("select Nom, Point_de_vie, niveau, nbAmes from Personnage inner join Stats on Personnage.Stat = Stats.idStats");
+        reader = bd.select("select Nom, Point_de_vie, niveau, nbAmes, Force, Defence, Vitesse from Personnage inner join Stats on Personnage.Stat = Stats.idStats");
         while(reader.Read())
         {
             string nom = reader.GetString(0);
             int hp = reader.GetInt32(1);
             int level = reader.GetInt32(2);
             int ames = reader.GetInt32(3);
-            listPers.Add(new Personnage(nom, hp, level, ames));
+            int force = reader.GetInt32(4);
+            int def = reader.GetInt32(5);
+            int speed = reader.GetInt32(6);
+            listPers.Add(new Personnage(nom, hp, level, ames, force, def, speed));
         }
         reader.Close();
         bd.Close();
+        listPers[0].Image = sp_hero;
+        listPers[1].Image = sp_Forest;
+        listPers[2].Image = sp_Fire;
+        listPers[3].Image = sp_Ice;
     }
 
     void Afficher()
@@ -49,7 +60,9 @@ public class MenuStatTeam : MonoBehaviour {
         foreach (GameObject obj in characterPanels)
         {
             List<Text> txt = new List<Text>(obj.GetComponentsInChildren<Text>());
-           
+            List<Image> img = new List<Image>(obj.GetComponentsInChildren<Image>());
+
+            img[1].sprite = listPers[i].Image;
             txt[0].text = listPers[i].Name;
             txt[1].text = "Niveau : " + listPers[i].Level.ToString();
             txt[2].text = "Hp : "  + listPers[i].Hp.ToString() + "/" + listPers[i].Hp.ToString();
@@ -59,40 +72,40 @@ public class MenuStatTeam : MonoBehaviour {
     }
 }
 
-public class Personnage
-{
-    private string name;
-    public string Name
-    {
-        get { return name; }
-        set { name = value; }
-    }
-    private int hp;
-    public int Hp
-    {
-        get { return hp; }
-        set { hp = value; }
-    }
-    private int level;
-    public int Level
-    {
-        get { return level; }
-        set { level = value; }
-    }
-    private int nbAmes;
-    public int NbAmes
-    {
-        get { return nbAmes; }
-        set { nbAmes = value; }
-    }
+//public class Personnage
+//{
+//    private string name;
+//    public string Name
+//    {
+//        get { return name; }
+//        set { name = value; }
+//    }
+//    private int hp;
+//    public int Hp
+//    {
+//        get { return hp; }
+//        set { hp = value; }
+//    }
+//    private int level;
+//    public int Level
+//    {
+//        get { return level; }
+//        set { level = value; }
+//    }
+//    private int nbAmes;
+//    public int NbAmes
+//    {
+//        get { return nbAmes; }
+//        set { nbAmes = value; }
+//    }
 
 
-    public Personnage(string name, int hp, int level, int nbAme)
-    {
-        this.Name = name;
-        this.Hp = hp;
-        this.Level = level;
-        this.NbAmes = nbAme;
-    }
-}
+//    public Personnage(string name, int hp, int level, int nbAme)
+//    {
+//        this.Name = name;
+//        this.Hp = hp;
+//        this.Level = level;
+//        this.NbAmes = nbAme;
+//    }
+//}
 
---
+++ Assets/Script/Personnage.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace Assets.Script
+{
+    class Personnage
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private int hp;
+        public int Hp
+        {
+            get { return hp; }
+            set { hp = value; }
+        }
+        private int level;
+        public int Level
+        {
+            get { return level; }
+            set { level = value; }
+        }
+        private int nbAmes;
+        public int NbAmes
+        {
+            get { return nbAmes; }
+            set { nbAmes = value; }
+        }
+        private int strength;
+        public int Strength
+        {
+            get { return strength; }
+            set { strength = value; }
+        }
+        private int defence;
+        public int Defence
+        {
+            get { return defence; }
+            set { defence = value; }
+        }
+        private int speed;
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+        private Sprite image;
+        public Sprite Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+        private bool defeated;
+        public bool isDefeated
+        {
+            get { return defeated; }
+            set { defeated = value; }
+        }
+
+
+        public Personnage(string name, int hp, int level, int nbAme, int str, int def, int sp)
+        {
+            Name = name;
+            Hp = hp;
+            Level = level;
+            NbAmes = nbAme;
+            Strength = str;
+            Defence = def;
+            Speed = sp;
+        }
+    }
+}
