f006c1fd849170f6ddc32c7c89bdbc5bb1c9e8a7
Unknown <louis.christophe.poulin@gmail.com>
2017-11-22 13:02:46 -0500

    menu Difficulté

--- Assets/Script/CombatUI.cs
+++ Assets/Script/CombatUI.cs
@@ -422,21 +422,44 @@ public class CombatUI : MonoBehaviour {
     public void ShowTooltipItem(int idBtn)
     {
         Color cl = bgtoolTipItem[idBtn].color;
+        AccesBD bd = new AccesBD();
+        SqliteDataReader reader;
+        string desc = "";
         cl.a = 255;
         bgtoolTipItem[idBtn].color = cl;
         switch(idBtn)
         {
             case 0:
-                toolTipItem[idBtn].text = " Potion Regeneration: +10hp";
+                reader = bd.select("select Description from Item where idItem = 3");
+                while(reader.Read())
+                {
+                    desc = reader.GetString(0);
+                }
+                toolTipItem[idBtn].text = desc;
                 break;
             case 1:
-                toolTipItem[idBtn].text = " Potion Steroids: +10Str";
+                reader = bd.select("select Description from Item where idItem = 7");
+                while (reader.Read())
+                {
+                    desc = reader.GetString(0);
+                }
+                toolTipItem[idBtn].text = desc;
                 break;
             case 2:
-                toolTipItem[idBtn].text = " Potion Harden: +10Def";
+                reader = bd.select("select Description from Item where idItem = 6");
+                while (reader.Read())
+                {
+                    desc = reader.GetString(0);
+                }
+                toolTipItem[idBtn].text = desc;
                 break;
             case 3:
-                toolTipItem[idBtn].text = " Potion Speed boost: +10Spd";
+                reader = bd.select("select Description from Item where idItem = 5");
+                while (reader.Read())
+                {
+                    desc = reader.GetString(0);
+                }
+                toolTipItem[idBtn].text = desc;
                 break;
         }
     }
--- Assets/Script/StartGame.cs
+++ Assets/Script/StartGame.cs
@@ -6,15 +6,39 @@ using Assets.Script;
 
 public class StartGame : MonoBehaviour {
 
+    public GameObject Btn_Diff;
+
 	public void LoadGame(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
     }
 
-    public void NewGame(int sceneIndex)
+    public void NewGame()
     {
         AccesBD bd = new AccesBD();
         bd.ClearBD();
-        SceneManager.LoadScene(sceneIndex);
+        GameObject.Find("Container_Btn_Main").SetActive(false);
+        Btn_Diff.SetActive(true);
+        //SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void chooseDiff(int i)
+    {
+        AccesBD bd = new AccesBD();
+        switch(i)
+        {
+            case 0:
+                bd.insert("Update Personnage set Niveau = 1 where idPersonnage = 1");
+                SceneManager.LoadScene(1);
+                break;
+            case 1:
+                bd.insert("Update Personnage set Niveau = 2 where idPersonnage = 1");
+                SceneManager.LoadScene(1);
+                break;
+            case 2:
+                bd.insert("Update Personnage set Niveau = 3 where idPersonnage = 1");
+                SceneManager.LoadScene(1);
+                break;
+        }
     }
 }
